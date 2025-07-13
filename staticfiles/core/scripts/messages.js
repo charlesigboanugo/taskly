1ffb4c348d58b3d41a60b25b70dc26a1ab1778bf
charles <charlesigboanugo033@gmail.com>
2025-07-13 14:59:39 +0100

    add dynamic 404 error page

--- staticfiles/core/scripts/messages.js
+++ staticfiles/core/scripts/messages.js
@@ -1,7 +1,9 @@
 export function initMessages() {
     const msg = document.querySelector('.messages');
+    const errlist = document.querySelectorAll('.errorlist');
 
     setTimeout(() => {
         if (msg) msg.classList.add('hide');
-    }, 5000);
+        errlist.forEach(el => el.classList.add('hide'));
+    }, 6000);
 }
