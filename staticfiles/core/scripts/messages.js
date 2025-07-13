8355831a8f3a5791f3e9a63d6c0f08d736b457cc
charles <charlesigboanugo033@gmail.com>
2025-07-13 15:57:02 +0100

    add timely scroll to error messages on page load

--- staticfiles/core/scripts/messages.js
+++ staticfiles/core/scripts/messages.js
@@ -4,6 +4,16 @@ export function initMessages() {
 
     setTimeout(() => {
         if (msg) msg.classList.add('hide');
-        errlist.forEach(el => el.classList.add('hide'));
     }, 6000);
+    
+
+    errlist.forEach((el, i) => {
+        setTimeout(() => {
+            el.scrollIntoView({ behavior: 'smooth', block: 'center' }); 
+        },i * 3000);
+    });
+
+    setTimeout(() => {
+        errlist.forEach(el => el.classList.add('hide'));
+    }, 15000);
 }
