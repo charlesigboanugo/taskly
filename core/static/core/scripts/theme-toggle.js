09dc36263020339ff3cdd4255f10e06d8b45505c
charles <charlesigboanugo033@gmail.com>
2025-07-13 11:40:32 +0100

    add dynamic 404 error page

--- core/static/core/scripts/theme-toggle.js
+++ core/static/core/scripts/theme-toggle.js
@@ -6,35 +6,42 @@ function storeTheme(val){
     localStorage.setItem('theme', val);
 }
 
+function styleToggles(data_theme) {
+    if (data_theme == "light"){
+        if (light_toggle)
+            light_toggle.classList.toggle("theme-active", true);
+        if (dark_toggle)
+            dark_toggle.classList.toggle("theme-active", false);
+    }
+    else if (data_theme == "dark"){
+        if (dark_toggle)
+            dark_toggle.classList.toggle("theme-active", true);
+        if (light_toggle)
+            light_toggle.classList.toggle("theme-active", false);
+    }
+}
 function darkMode(){
     const data_theme = html.getAttribute("data-theme");
     if (data_theme == "light" || data_theme == null){
-     html.setAttribute("data-theme", "dark");
-        if (light_toggle)
-            light_toggle.classList.toggle("theme-active", false);
-        if (dark_toggle)
-        {
-            dark_toggle.classList.toggle("theme-active", true);
-            storeTheme("dark");
-        }
+        html.setAttribute("data-theme", "dark");
+        styleToggles("dark");
     }
+    storeTheme("dark");
 }
 
 function lightMode(){
     const data_theme = html.getAttribute("data-theme");
     if (data_theme == "dark" || data_theme == null){
-     html.setAttribute("data-theme", "light");
-        if (dark_toggle)
-            dark_toggle.classList.toggle("theme-active", false);
-        if (light_toggle)
-        {
-            light_toggle.classList.toggle("theme-active", true);
-            storeTheme("light");
-        }
+        html.setAttribute("data-theme", "light");
+        styleToggles("light");
     }
+    storeTheme("light");
 }
 
 export function initThemeToggle() {
+    const data_theme = html.getAttribute("data-theme");
+
+    styleToggles(data_theme);
     dark_toggle.addEventListener("click", darkMode);
     light_toggle.addEventListener("click", lightMode);
 }
