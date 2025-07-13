9fc8387ad2aaccd38dc9ed47a77a8a0952c18314
charles <charlesigboanugo033@gmail.com>
2025-07-13 10:29:33 +0100

    slight changes

--- staticfiles/core/scripts/theme-toggle.js
+++ staticfiles/core/scripts/theme-toggle.js
@@ -1,4 +1,4 @@
-const pageBody = document.body;
+const html = document.documentElement;
 const dark_toggle = document.querySelector("[data-toggle='theme-dark']");
 const light_toggle = document.querySelector("[data-toggle='theme-light']");
     
@@ -6,14 +6,10 @@ function storeTheme(val){
     localStorage.setItem('theme', val);
 }
 
-function defaultTheme(){
-    return localStorage.getItem('theme');
-}
-
 function darkMode(){
-    const data_theme = pageBody.getAttribute("data-theme");
+    const data_theme = html.getAttribute("data-theme");
     if (data_theme == "light" || data_theme == null){
-        pageBody.setAttribute("data-theme", "dark");
+     html.setAttribute("data-theme", "dark");
         if (light_toggle)
             light_toggle.classList.toggle("theme-active", false);
         if (dark_toggle)
@@ -25,9 +21,9 @@ function darkMode(){
 }
 
 function lightMode(){
-    const data_theme = pageBody.getAttribute("data-theme");
+    const data_theme = html.getAttribute("data-theme");
     if (data_theme == "dark" || data_theme == null){
-        pageBody.setAttribute("data-theme", "light");
+     html.setAttribute("data-theme", "light");
         if (dark_toggle)
             dark_toggle.classList.toggle("theme-active", false);
         if (light_toggle)
@@ -38,17 +34,7 @@ function lightMode(){
     }
 }
 
-export function setTheme(){
-    const data_theme = defaultTheme();
-    if (data_theme == null || data_theme == "dark")
-        darkMode();
-    else{
-        lightMode();
-    }
-}
-
 export function initThemeToggle() {
-    setTheme();
     dark_toggle.addEventListener("click", darkMode);
     light_toggle.addEventListener("click", lightMode);
 }
